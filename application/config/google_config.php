4b276d9bbdad37fff7c825e8c1b0f6d305d88bc6
rizkyfalih <rizkyfalihpridyandhika@ymail.com>
2018-11-09 11:32:40 +0700

    first push

---
+++ application/config/google_config.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['google_client_id']="671848186721-4chme71kis6blcdrl62q14e824b6eqpf.apps.googleusercontent.com";
+$config['google_client_secret']="_S8vSNIw5gIHNoQGrp0WQjPU";
+$config['google_redirect_url']='http://localhost/Fintech/';
+
